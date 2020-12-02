a0840ec3fbfc1b11b24ee2f0939be14e96421ece
Richard Kotze <richkotze@outlook.com>
2020-12-02 18:13:44 +0000

    Solved day 1 puzzle 2

--- src/day1.js
+++ src/day1.js
@@ -2,19 +2,43 @@ var fs = require("fs");
 var path = require("path");
 
 function puzzle1() {
-  const data = fs.readFileSync(path.join(__dirname, "./data/day.txt"), { encoding: 'utf8'});
-  const list = data.split("\n");
-  let result = 0;
+  const list = fetchData();
   for (const num1 of list) {
     for (const num2 of list) {
-      const first = Number(num1)
-      const second = Number(num2);
-      if((first + second) === 2020){
-        result = first * second;
+      const val = multiplyWhen2020(Number(num1), Number(num2));
+      if(val > 0) return val;
+    }
+  }
+  return 0;
+}
+
+function puzzle2() {
+  const list = fetchData();
+  for (const num1 of list) {
+    for (const num2 of list) {
+      for (const num3 of list) {
+        const val = multiplyWhen2020(Number(num1), Number(num2), Number(num3));
+        if(val > 0) return val
       }
     }
   }
-  return result;
+  return 0;
+}
+
+function multiplyWhen2020(...values){
+  const total = values.reduce((total, cur) => total + cur, 0);
+  if(total === 2020){
+    return values.reduce((total, cur) => total * cur, 1);
+  }
+  return 0;
+}
+
+function fetchData() {
+  const data = fs.readFileSync(path.join(__dirname, "./data/day.txt"), { encoding: 'utf8' });
+  const list = data.split("\n");
+  return list;
 }
 
 exports.puzzle1 = puzzle1;
+exports.puzzle2 = puzzle2;
+
--- src/index.js
+++ src/index.js
@@ -3,8 +3,11 @@ let day1 = require("./day1");
 let myArgs = process.argv.slice(2);
 
 switch(myArgs[0]) {
-  case "1":
+  case "1.1":
     console.log(day1.puzzle1());
+    break;
+  case "1.2":
+    console.log(day1.puzzle2());
     break;
   default:
     console.log("No advent found");
