3134f77d2c4fe507de61c732b65eb153a8b8b420
Richard Kotze <richkotze@outlook.com>
2020-12-06 12:28:19 +0000

    Solved Day 6 part 2

--- src/day6.js
+++ src/day6.js
@@ -16,4 +16,26 @@ function puzzle1() {
   return total;
 }
 
+function puzzle2() {
+  const data = fileReader("day6.txt");
+  const groups = data.split("\n\n");
+  let total = 0;
+  for (const group of groups) {
+    const people = group.split("\n");
+    let firstAnswers = people[0].split("");
+    if (people.length === 1) {
+      total += firstAnswers.length;
+      continue;
+    }
+
+    for (const answer of firstAnswers) {
+      if (people.every((answers) => answers.includes(answer))) {
+        total += 1;
+      }
+    }
+  }
+  return total;
+}
+
 console.log(puzzle1());
+console.log(puzzle2());
