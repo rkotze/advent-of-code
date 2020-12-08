ec76fc4952dc774e133fb1fc7a4874f2d6651aad
Richard Kotze <richkotze@outlook.com>
2020-12-08 17:48:20 +0000

    Refactor Day 7
    Could not help myself

--- src/day7.js
+++ src/day7.js
@@ -3,24 +3,7 @@ const { fileReader } = require("./file-reader");
 function puzzle1() {
   const data = fileReader("day7.txt");
   const bagsOfBags = data.split("\n");
-  const baggage = {};
-
-  for (const bagsList of bagsOfBags) {
-    const [containerBag, innerBags] = bagsList.split(" contain ");
-    const bag = containerBag.slice(0, containerBag.length - 1);
-    if (!innerBags) return "Empty error: " + bagsList;
-
-    const childBag = innerBags
-      .match(/([a-z\s]+)bag/gi)
-      .map((bag) => bag.trim());
-    if (!baggage[bag]) {
-      if (childBag.includes("no other bag")) {
-        baggage[bag] = [];
-      } else {
-        baggage[bag] = childBag;
-      }
-    }
-  }
+  const baggage = buildBaggage(bagsOfBags);
   let count = 0;
   for (const lookInBag of Object.keys(baggage)) {
     if (hasShinyGold(baggage, lookInBag)) count++;
@@ -31,12 +14,13 @@ function puzzle1() {
 function hasShinyGold(baggage, lookIn) {
   const innerBags = baggage[lookIn];
   if (innerBags) {
-    if (innerBags.includes("shiny gold bag")) {
+    if (innerBags.join(" ").includes("shiny gold bag")) {
       return true;
     } else {
       const look = [];
       for (const innerBag of innerBags) {
-        look.push(hasShinyGold(baggage, innerBag));
+        const { name } = getBagInfo(innerBag);
+        look.push(hasShinyGold(baggage, name));
       }
       return look.some((val) => val);
     }
@@ -48,6 +32,33 @@ function hasShinyGold(baggage, lookIn) {
 function puzzle2() {
   const data = fileReader("day7.txt");
   const bagsOfBags = data.split("\n");
+
+  return sumBags(buildBaggage(bagsOfBags)) - 1; // minus the gold bag
+}
+
+function sumBags(baggage, lookIn = "shiny gold bag") {
+  const innerBags = baggage[lookIn];
+  let total = 1;
+  if (innerBags) {
+    for (const bagTotal of innerBags) {
+      const { count, name } = getBagInfo(bagTotal);
+
+      total += Number(count) * sumBags(baggage, name);
+    }
+  }
+
+  return total;
+}
+
+function getBagInfo(bagTotal) {
+  const [, count, name] = bagTotal.match(/(\d+)\s([a-z\s]+)/i);
+  return {
+    count,
+    name,
+  };
+}
+
+function buildBaggage(bagsOfBags) {
   const baggage = {};
 
   for (const bagsList of bagsOfBags) {
@@ -66,22 +77,7 @@ function puzzle2() {
       }
     }
   }
-
-  return sumBags(baggage) - 1; // minus the gold bag
-}
-
-function sumBags(baggage, lookIn = "shiny gold bag") {
-  const innerBags = baggage[lookIn];
-  let total = 1;
-  if (innerBags) {
-    for (const bagTotal of innerBags) {
-      const [, count, name] = bagTotal.match(/(\d+)\s([a-z\s]+)/i);
-
-      total += Number(count) * sumBags(baggage, name);
-    }
-  }
-
-  return total;
+  return baggage;
 }
 
 console.log(puzzle1());
