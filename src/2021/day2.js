2f6846dd65ed14264b391ac2b71f1292c564d74b
Richard Kotze <richkotze@outlook.com>
2021-12-11 16:20:19 +0000

    :rocket: Day 2 part 2 complete

--- src/2021/day2.js
+++ src/2021/day2.js
@@ -20,3 +20,27 @@ function puzzle1() {
 }
 
 console.log(puzzle1());
+
+function puzzle2() {
+  const list = readPuzzle("2021", "day2.txt");
+  let h = 0;
+  let d = 0;
+  let aim = 0;
+  for (const instruction of list) {
+    const [direction, unit] = instruction.split(/\s/g);
+    const v = Number(unit);
+    if (direction === "forward") {
+      h += v;
+      if (aim !== 0) d += aim * v;
+    }
+    if (direction === "down") {
+      aim += v;
+    }
+    if (direction === "up") {
+      aim -= v;
+    }
+  }
+  return h * d;
+}
+
+console.log(puzzle2());
