6a7d383e4fc8afbafdc313237076c24990b14a18
Richard Kotze <richkotze@outlook.com>
2021-12-15 18:34:41 +0000

    :white_check_mark: Day 4 part 1 complete

--- src/2021/day4.js
+++ src/2021/day4.js
@@ -2,23 +2,44 @@ const { readPuzzle } = require("../file-reader");
 
 function puzzle1() {
   const { numbers, boards } = buildGame();
-  buildCheckboards(boards);
-  // let count = 0;
-  // let previousNum = 0;
-  // for (const num of list) {
-  //   let depth = Number(num);
-  //   if (previousNum !== 0 && depth > previousNum) {
-  //     count++;
-  //   }
-  //   previousNum = depth;
-  // }
-  // return count;
+  const checkboards = buildCheckboards(boards);
+  for (let i = 0; i < numbers.length; i++) {
+    const number = numbers[i];
+    for (const board of checkboards) {
+      for (let j = 0; j < board.rows.length; j++) {
+        const row = board.rows[j];
+        const col = board.columns[j];
+        const rowIndex = row.indexOf(number);
+        const colIndex = col.indexOf(number);
+        if (rowIndex > -1) {
+          row[rowIndex] = "x";
+        }
+        if (colIndex > -1) {
+          col[colIndex] = "x";
+        }
+        if (checkForWinner(row) || checkForWinner(col)) {
+          return sumBoard(board.rows) * number;
+        }
+      }
+    }
+  }
 }
 
 console.log(puzzle1());
 
+function checkForWinner(arr) {
+  return arr.every((val) => val === "x");
+}
+
+function sumBoard(board) {
+  return board.reduce((acc, nxt) => {
+    acc += nxt.reduce((acc, nxt) => (acc += nxt != "x" ? Number(nxt) : 0), 0);
+    return acc;
+  }, 0);
+}
+
 function buildGame() {
-  const list = readPuzzle("2021", "day4-1t.txt", (data) => {
+  const list = readPuzzle("2021", "day4.txt", (data) => {
     return data.split(/\n\n/g);
   });
 
@@ -52,4 +73,5 @@ function buildCheckboards(boards) {
       columns,
     });
   }
+  return checkBoards;
 }
