271845fa6769dc3850a124b62c4b6a6db5331b42
Richard Kotze <richkotze@outlook.com>
2021-12-17 18:58:27 +0000

    :white_check_mark: Day 5 part 2 complete

--- src/2021/day5.js
+++ src/2021/day5.js
@@ -42,3 +42,58 @@ function puzzle1() {
 }
 
 console.log(puzzle1());
+
+function puzzle2() {
+  const list = readPuzzle("2021", "day5.txt", (data) => {
+    const lines = data.split(/\n/g);
+    return lines.map((line) => {
+      return line.split(/\s->\s/g);
+    });
+  });
+
+  const path = new Map();
+
+  for (const [starts, ends] of list) {
+    const [x1s, y1s] = starts.split(",");
+    const [x2s, y2s] = ends.split(",");
+    const x1 = Number(x1s);
+    const x2 = Number(x2s);
+    const y1 = Number(y1s);
+    const y2 = Number(y2s);
+    let start = [x1, y1];
+    let end = [x2, y2];
+
+    countOverlap(start, path);
+    do {
+      start = step(start, end);
+      countOverlap(start, path);
+    } while (!hasTraveled(start, end));
+  }
+
+  return Array.from(path.values()).filter((val) => val >= 2).length;
+}
+
+console.log(puzzle2());
+
+function countOverlap(start, path) {
+  const [kx1, ky1] = start;
+  let key = `${kx1}:${ky1}`;
+  let val = path.get(key) || 0;
+  path.set(key, val + 1);
+}
+
+function step(start, end) {
+  const [x1, y1] = start;
+  const [x2, y2] = end;
+  let x = x1 < x2 ? x1 + 1 : x1 - 1;
+  let y = y1 < y2 ? y1 + 1 : y1 - 1;
+  if (x1 === x2) x = x1;
+  if (y1 === y2) y = y1;
+  return [x, y];
+}
+
+function hasTraveled(start, end) {
+  const [x1, y1] = start;
+  const [x2, y2] = end;
+  return x1 === x2 && y1 === y2;
+}
