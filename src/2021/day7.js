db11ef367d214dfa7b8858542d887b08a83bba2f
Richard Kotze <richkotze@outlook.com>
2021-12-18 21:22:18 +0000

    :white_check_mark: day 7 part 2 complete

--- src/2021/day7.js
+++ src/2021/day7.js
@@ -21,3 +21,30 @@ function puzzle1() {
 }
 
 console.log(puzzle1());
+
+function puzzle2() {
+  const list = readPuzzle("2021", "day7.txt", (data) =>
+    data.split(",").map((data) => Number(data))
+  );
+
+  const fuelTracker = new Map();
+  const max = Math.max(...list);
+  for (let i = 0; i < max; i++) {
+    const crab = i;
+    if (fuelTracker.has(crab)) continue;
+    let totalFuel = 0;
+    for (const num of list) {
+      const fuel = Math.abs(num - crab);
+      totalFuel += sumN(fuel);
+    }
+    fuelTracker.set(crab, totalFuel);
+  }
+
+  return Math.min(...Array.from(fuelTracker.values()));
+}
+
+function sumN(n) {
+  return (n * (n + 1)) / 2;
+}
+
+console.log(puzzle2());
