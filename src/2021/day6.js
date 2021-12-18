abf1dfe9fea7400495552d6c0a23dfb89f00a975
Richard Kotze <richkotze@outlook.com>
2021-12-18 14:17:43 +0000

    :white_check_mark: Day 6 part 2 complete

--- src/2021/day6.js
+++ src/2021/day6.js
@@ -1,6 +1,6 @@
 const { readPuzzle } = require("../file-reader");
 
-function puzzle1(days = 18) {
+function puzzle1(days) {
   const list = readPuzzle("2021", "day6.txt", (data) =>
     data.split(",").map((num) => Number(num))
   );
@@ -21,3 +21,35 @@ function puzzle1(days = 18) {
 }
 
 console.log(puzzle1(80));
+
+function puzzle2(days) {
+  const shoal = readPuzzle("2021", "day6.txt", (data) =>
+    data.split(",").map((num) => Number(num))
+  );
+
+  const fishSpans = Array(9).fill(0);
+  for (const age of shoal) {
+    fishSpans[age]++;
+  }
+
+  for (let i = 0; i < days; i++) {
+    let ageSix = 0;
+    let ageEight = 0;
+    for (let age = 0; age < fishSpans.length; age++) {
+      let count = fishSpans[age];
+      if (age === 0) {
+        ageSix = count;
+        ageEight = count;
+      } else {
+        fishSpans[age - 1] = count;
+      }
+    }
+
+    fishSpans[6] += ageSix;
+    fishSpans[8] = ageEight;
+  }
+
+  return fishSpans.reduce((acc, cur) => acc + cur, 0);
+}
+
+console.log(puzzle2(256));
