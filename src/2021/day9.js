3b6afa5aa2ff2a6df9bf2119a6563be38c16ec26
Richard Kotze <richkotze@outlook.com>
2021-12-31 17:38:02 +0000

    :white_check_mark: Day 9 part 2 complete

--- src/2021/day9.js
+++ src/2021/day9.js
@@ -12,10 +12,10 @@ function puzzle1() {
   for (let y = 0; y < list.length; y++) {
     const row = list[y];
     for (let x = 0; x < row.length; x++) {
-      const basin = resolveBasin(y, x, list, row[x]);
+      const basin = resolveLowPoint(y, x, list, row[x]);
       if (basin) {
-        const [key, val] = basin;
-        lowest[key] = val;
+        const [y, x, val] = basin;
+        lowest[y + " " + x] = val;
       }
     }
   }
@@ -23,7 +23,7 @@ function puzzle1() {
   return Object.values(lowest).reduce((acc, cur) => (acc += cur + 1), 0);
 }
 
-function resolveBasin(y, x, list, low = 9) {
+function resolveLowPoint(y, x, list, low = 9) {
   let pos = {};
   list[y - 1] != undefined ? (pos[list[y - 1][x]] = [y - 1, x]) : null;
   list[y + 1] != undefined ? (pos[list[y + 1][x]] = [y + 1, x]) : null;
@@ -35,7 +35,67 @@ function resolveBasin(y, x, list, low = 9) {
     return null;
   }
 
-  return [y + " " + x, low];
+  return [y, x, low];
 }
 
 console.log(puzzle1());
+
+function puzzle2() {
+  const list = readPuzzle("2021", "day9.txt", (data) => {
+    return data
+      .split(/\n/g)
+      .map((row) => row.split("").map((num) => Number(num)));
+  });
+
+  const basinSizes = [];
+
+  for (let y = 0; y < list.length; y++) {
+    const row = list[y];
+    for (let x = 0; x < row.length; x++) {
+      const lowPoint = resolveLowPoint(y, x, list, row[x]);
+      if (lowPoint) {
+        const [y, x, _] = lowPoint;
+        basinSizes.push(basinSize(y, x, list));
+      }
+    }
+  }
+
+  return basinSizes
+    .sort((a, b) => a - b)
+    .slice(-3)
+    .reduce((acc, cur) => acc * cur, 1);
+}
+
+function basinSize(y, x, list) {
+  const basin = [];
+  const queue = [[y, x]];
+
+  while (queue.length > 0) {
+    const [y, x] = queue.pop();
+    const val = nodeValue(y, x, list);
+    const key = nodeKey(y, x);
+    if (val < 9 && !basin.includes(key)) {
+      basin.push(key);
+      queue.push([y - 1, x]);
+      queue.push([y + 1, x]);
+      queue.push([y, x - 1]);
+      queue.push([y, x + 1]);
+    }
+  }
+  return basin.length;
+}
+
+function nodeKey(y, x) {
+  return y + " " + x;
+}
+
+function nodeValue(y, x, list) {
+  const valY = list[y];
+  if (valY === undefined) return 9;
+  let valX = valY[x];
+  if (valX === undefined) return 9;
+
+  return valX;
+}
+
+console.log(puzzle2());
