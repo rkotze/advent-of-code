b483a9e5399be433a7c0070b92926390f9c3f30d
Richard Kotze <richkotze@outlook.com>
2022-11-20 13:58:35 +0000

    Build a undirection graph
    Handle visited components with set.
    Why use a Set? Because sets are O(1) time, array look up O(n) time

--- src/tools/graph.js
+++ src/tools/graph.js
@@ -2,28 +2,52 @@
 // follows a direct path
 function depthFirst(graph, src, cb) {
   const stack = [src];
+  // sets are O(1) time, array look up O(n) time
+  const visited = new Set();
 
   while (stack.length > 0) {
     const current = stack.pop();
+    visited.add(current);
     if (cb(current) === true) return true;
     for (const next of graph[current]) {
-      stack.push(next);
+      if (!visited.has(next)) {
+        stack.push(next);
+      }
     }
   }
 }
 
 // Breath first can NOT be recursive
 // Looks at all immediate path for a node
-function breathFirst(graph, src, cb) {
+function breadthFirst(graph, src, cb) {
   const queue = [src];
+  // sets are O(1) time, array look up O(n) time
+  const visited = new Set();
 
   while (queue.length > 0) {
     const current = queue.shift();
+    visited.add(current);
     if (cb(current) === true) return true;
     for (const next of graph[current]) {
-      queue.push(next);
+      if (!visited.has(next)) {
+        queue.push(next);
+      }
     }
   }
 }
 
-module.exports = { depthFirst, breathFirst };
+function buildGraph(edges) {
+  const graph = {};
+
+  for (let edge of edges) {
+    const [a, b] = edge;
+    if (!(a in graph)) graph[a] = [];
+    if (!(b in graph)) graph[b] = [];
+    graph[a].push(b);
+    graph[b].push(a);
+  }
+
+  return graph;
+}
+
+module.exports = { depthFirst, breadthFirst, buildGraph };
--- src/tools/graph.test.js
+++ src/tools/graph.test.js
@@ -1,5 +1,5 @@
-// const assert = require("assert");
-const { depthFirst, breathFirst } = require("./graph");
+const assert = require("assert");
+const { depthFirst, breadthFirst, buildGraph } = require("./graph");
 // Adjacent object
 // acycle
 const aCycle = {
@@ -18,7 +18,37 @@ depthFirst(aCycle, "a", (value) => {
 }); // a,c,e,b
 
 console.log("Breath");
-breathFirst(aCycle, "a", (value) => {
+breadthFirst(aCycle, "a", (value) => {
   console.log(value);
   return value === "e";
 }); // a,b,c,d,e
+
+const edges = [
+  ["i", "j"],
+  ["k", "i"],
+  ["m", "k"],
+  ["k", "l"],
+  ["o", "n"],
+];
+// console.log(buildGraph(edges));
+
+const undirectedPath = (edges, nodeA, nodeB) => {
+  const graph = buildGraph(edges);
+  return hasPath(graph, nodeA, nodeB);
+};
+
+const hasPath = (graph, src, dst) => {
+  if (
+    depthFirst(graph, src, (value) => {
+      return value === dst;
+    }) === true
+  ) {
+    return true;
+  }
+  return false;
+};
+
+console.log("test: j->m true");
+assert(undirectedPath(edges, "j", "m") === true, "j->m should be true"); // -> true
+console.log("test: k->o false");
+assert(undirectedPath(edges, "k", "o") === false, "k->o should be false"); // -> false
