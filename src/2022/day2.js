112e1abc5753807de126b70681573087e71af91e
Richard Kotze <richkotze@outlook.com>
2022-12-02 17:33:56 +0000

    Day 2 part 2 done

--- src/2022/day2.js
+++ src/2022/day2.js
@@ -32,3 +32,32 @@ function puzzle1() {
 }
 
 console.log(puzzle1());
+
+const win = {
+  A: "B",
+  B: "C",
+  C: "A",
+};
+
+const lose = {
+  A: "C",
+  B: "A",
+  C: "B",
+};
+
+function resolve(a, outcome) {
+  if (outcome === "Y") return playerA[a] + 3;
+  if (outcome === "X") return playerA[lose[a]];
+  return playerA[win[a]] + 6;
+}
+
+function puzzle2() {
+  const data = readPuzzle("2022", "day2.txt");
+  let total = 0;
+  for (const shapes of data) {
+    total += resolve(...shapes.split(" "));
+  }
+  return total;
+}
+
+console.log(puzzle2());
