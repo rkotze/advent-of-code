08a550b7a16122c39616f8d0b74ba0bf893a8e41
Richard Kotze <richkotze@outlook.com>
2022-12-03 09:13:17 +0000

    Day 3 part 2

--- src/2022/day3.js
+++ src/2022/day3.js
@@ -22,3 +22,35 @@ function puzzle1() {
 }
 
 console.log(puzzle1());
+
+function puzzle2() {
+  const data = readPuzzle("2022", "day3.txt", (data) => {
+    const all = data.split(/\n/g);
+    const groups = [];
+    for (let i = 0; i < all.length; i += 3) {
+      groups.push([
+        all[i].split(""),
+        all[i + 1].split(""),
+        all[i + 2].split(""),
+      ]);
+    }
+    return groups;
+  });
+  let total = 0;
+  for (const bags of data) {
+    const [bag1, bag2, bag3] = bags;
+    for (const char of bag1) {
+      if (findChar(bag2, char) && findChar(bag3, char)) {
+        total += priority(char);
+        break;
+      }
+    }
+  }
+  return total;
+}
+
+function findChar(list, char) {
+  return list.some((c) => char === c);
+}
+
+console.log(puzzle2());
