bf9c6609f15ccc89c916162bd04c19606acea8c3
Richard Kotze <richkotze@outlook.com>
2022-12-05 21:14:17 +0000

    Day 5 part 1

---
+++ src/2022/day5.js
@@ -0,0 +1,64 @@
+const { readPuzzle } = require("../file-reader");
+
+function puzzle1() {
+  const data = readPuzzle("2022", "day5.txt", (data) => {
+    const rows = data.split(/\n/g);
+    const crates = [];
+    let next = 0;
+
+    for (let i = 0; i < rows.length; i++) {
+      if (rows[i].includes("1")) {
+        next = i + 2;
+        break;
+      }
+      const splinters = rows[i].split("");
+      const baseCount = 4;
+      let cratePos = 0;
+      for (let j = 0; j < splinters.length; j += baseCount) {
+        const crate = splinters[j + 1];
+        if (/\s/.test(crate)) {
+          cratePos += 1;
+          continue;
+        }
+        if (Array.isArray(crates[cratePos])) {
+          crates[cratePos].push(crate);
+        } else {
+          crates[cratePos] = [crate];
+        }
+        cratePos += 1;
+      }
+    }
+
+    // console.log("end of creates", crates);
+    const moves = [];
+    for (let i = next; i < rows.length; i++) {
+      const move = rows[i];
+      moves.push(move.match(/(\d+)/g).map((num) => Number(num)));
+    }
+    // console.log(moves);
+    return {
+      crates,
+      moves,
+    };
+  });
+
+  const { crates, moves } = data;
+  for (const [steps, from, to] of moves) {
+    for (let i = 0; i < steps; i++) {
+      const stack = crates[from - 1];
+
+      if (stack.length > 0) {
+        let crate = stack.shift();
+        crates[to - 1].unshift(crate);
+      }
+    }
+  }
+
+  const topCrates = [];
+  for (const stack of crates) {
+    topCrates.push(stack.shift());
+  }
+  return topCrates.join("");
+}
+
+console.log(puzzle1());
