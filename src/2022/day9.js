f34ca8fcba1bba9d3bb2a39bf60d2a05e1cbf52a
Richard Kotze <richkotze@outlook.com>
2022-12-10 12:33:18 +0000

    Todo Day 9

---
+++ src/2022/day9.js
@@ -0,0 +1,10 @@
+const { readPuzzle } = require("../file-reader");
+
+function puzzle1() {
+  const data = readPuzzle("2022", "day9t.txt");
+  for (const num1 of data) {
+  }
+  return 0;
+}
+
+console.log(puzzle1());
