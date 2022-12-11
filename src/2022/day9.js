cbf51b94b34aa2d699a9ff639223644daf20bccf
Richard Kotze <richkotze@outlook.com>
2022-12-11 14:50:26 +0000

    Day 9 part 1

--- src/2022/day9.js
+++ src/2022/day9.js
@@ -1,10 +1,51 @@
 const { readPuzzle } = require("../file-reader");
 
 function puzzle1() {
-  const data = readPuzzle("2022", "day9t.txt");
-  for (const num1 of data) {
+  const data = readPuzzle("2022", "day9.txt");
+  let posH = [0, 0];
+  let posT = [0, 0];
+  const trackTail = new Map();
+
+  for (const strDir of data) {
+    const [dir, steps] = strDir.split(" ");
+    let newPos = moveHT(trackTail, [dir, Number(steps)], posH, posT);
+    posH = newPos[0];
+    posT = newPos[1];
   }
-  return 0;
+
+  return trackTail.size;
+}
+
+function moveHT(track, [dir, steps], [hy, hx], [ty, tx], step = 0) {
+  const key = [ty, tx].join(":");
+  if (!track.has(key)) track.set(key, 1);
+  if (steps === step)
+    return [
+      [hy, hx],
+      [ty, tx],
+    ];
+  if (dir === "R") {
+    const t = moveT([hy, hx + 1], [ty, tx]) ? [hy, tx + 1] : [ty, tx];
+    return moveHT(track, [dir, steps], [hy, hx + 1], t, step + 1);
+  }
+  if (dir === "U") {
+    const t = moveT([hy - 1, hx], [ty, tx]) ? [ty - 1, hx] : [ty, tx];
+    return moveHT(track, [dir, steps], [hy - 1, hx], t, step + 1);
+  }
+  if (dir === "L") {
+    const t = moveT([hy, hx - 1], [ty, tx]) ? [hy, tx - 1] : [ty, tx];
+    return moveHT(track, [dir, steps], [hy, hx - 1], t, step + 1);
+  }
+  if (dir === "D") {
+    const t = moveT([hy + 1, hx], [ty, tx]) ? [ty + 1, hx] : [ty, tx];
+    return moveHT(track, [dir, steps], [hy + 1, hx], t, step + 1);
+  }
+}
+
+function moveT([hy, hx], [ty, tx]) {
+  const y = Math.abs(hy - ty);
+  const x = Math.abs(hx - tx);
+  return y > 1 || x > 1;
 }
 
 console.log(puzzle1());
