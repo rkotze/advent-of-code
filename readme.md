c3f91863cca445408e762acc4565c53c96fc08f5
Richard Kotze <richkotze@outlook.com>
2023-12-02 00:50:46 +0000

    Day 1 part 2 completed

--- readme.md
+++ readme.md
@@ -16,8 +16,9 @@ Run code: `node src/2022/day1.js`
 
 ## 2023 Solved
 
-1. :white_check_mark: :black_square_button: [Day 1](https://adventofcode.com/2023/day/1)
+1. :white_check_mark: :white_check_mark: [Day 1](https://adventofcode.com/2023/day/1)
 2. :black_square_button: :black_square_button: [Day 2](https://adventofcode.com/2023/day/2)
+3. :black_square_button: :black_square_button: [Day 3](https://adventofcode.com/2023/day/3)
 
 ## 2022 Solved
 
--- src/2023/day1.js
+++ src/2023/day1.js
@@ -2,17 +2,47 @@ const { readPuzzle } = require("../file-reader");
 
 function puzzle1() {
   const data = readPuzzle("2023", "day1.txt");
-  const cali = [];
+  let total = 0;
   for (const newDesign of data) {
     const nums = newDesign.match(/\d/g);
-    if (nums.length == 1) cali.push(nums[0] + nums[0]);
-    if (nums.length == 2) cali.push(nums[0] + nums[1]);
-    if (nums.length > 2) cali.push(nums[0] + nums[nums.length - 1]);
+    total += Number(nums.at(0) + nums.at(-1));
   }
 
-  return cali.reduce((a, b) => {
-    return a + Number(b);
-  }, 0);
+  return total;
 }
 
 console.log(puzzle1());
+
+function puzzle2() {
+  const data = readPuzzle("2023", "day1.txt");
+  let total = 0;
+  for (const newDesign of data) {
+    const newFormat = updateWords(newDesign);
+    const nums = newFormat.match(/\d/g);
+    total += Number(nums.at(0) + nums.at(-1));
+  }
+
+  return total;
+}
+
+console.log(puzzle2());
+
+function updateWords(str) {
+  const word2num = {
+    one: "o1e",
+    two: "t2o",
+    three: "t3e",
+    four: "f4r",
+    five: "f5e",
+    six: "s6x",
+    seven: "s7n",
+    eight: "e8t",
+    nine: "n9e",
+  };
+
+  const keys = Object.keys(word2num);
+  for (const key of keys) {
+    str = str.replace(new RegExp(key, "gi"), word2num[key]);
+  }
+  return str;
+}
