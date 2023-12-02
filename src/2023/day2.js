62d07265820b239a75e90eda1b0068e8b20e4fe5
Richard Kotze <richkotze@outlook.com>
2023-12-02 12:23:50 +0000

    Day 2 part 2 done

--- src/2023/day2.js
+++ src/2023/day2.js
@@ -31,3 +31,34 @@ function puzzle1() {
 }
 
 console.log(puzzle1());
+
+function puzzle2() {
+  const data = readPuzzle("2023", "day2.txt");
+  const regBlue = /(\d+)\sblue/g;
+  const regRed = /(\d+)\s(red)/g;
+  const regGreen = /(\d+)\s(green)/g;
+  let total = 0;
+  for (let index = 0; index < data.length; index++) {
+    const game = data[index];
+    let cubes = [
+      ...game.match(regBlue),
+      ...game.match(regRed),
+      ...game.match(regGreen),
+    ];
+
+    const red = [];
+    const green = [];
+    const blue = [];
+    for (const cube of cubes) {
+      const split = cube.split(" ");
+      if (cube.includes("red")) red.push(Number(split[0]));
+      if (cube.includes("green")) green.push(Number(split[0]));
+      if (cube.includes("blue")) blue.push(Number(split[0]));
+    }
+
+    total += Math.max(...red) * Math.max(...green) * Math.max(...blue);
+  }
+  return total;
+}
+
+console.log(puzzle2());
