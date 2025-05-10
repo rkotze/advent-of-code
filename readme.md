7c8da7a5ad8e7f31c2fbf0fb353c369a454b86ec
Richard Kotze <richkotze@outlook.com>
2025-05-10 22:30:35 +0100

    Solved day 1.2

--- readme.md
+++ readme.md
@@ -16,7 +16,8 @@ Run code: `node src/2022/day1.js`
 
 ## 2024 Solved
 
-1. :white_check_mark: :black_square_button: [Day 1](https://adventofcode.com/2024/day/1)
+1. :white_check_mark: :white_check_mark: [Day 1](https://adventofcode.com/2024/day/1)
+2. :black_square_button: :black_square_button: [Day 2](https://adventofcode.com/2024/day/2)
    
 ## 2023 Solved
 
--- src/2024/day1.js
+++ src/2024/day1.js
@@ -1,7 +1,7 @@
 const { readPuzzle } = require("../file-reader");
 
 function puzzle1() {
-  const data = readPuzzle("2024", "day1t.txt", (data) => {
+  const data = readPuzzle("2024", "day1.txt", (data) => {
     const twoNums = [[], []];
     data.split(/\n/g).forEach((twoCols) => {
       const [one, two] = twoCols.split(/\s+/g);
@@ -24,3 +24,35 @@ function puzzle1() {
 }
 
 console.log(puzzle1());
+
+function puzzle2() {
+  const data = readPuzzle("2024", "day1.txt", (data) => {
+    const twoNums = [[], []];
+    data.split(/\n/g).forEach((twoCols) => {
+      const [one, two] = twoCols.split(/\s+/g);
+      twoNums[0].push(Number(one));
+      twoNums[1].push(Number(two));
+    });
+    return twoNums;
+  });
+
+  const countOcc = {};
+  for (const rNum of data[1]) {
+    if (countOcc[rNum] !== undefined) {
+      countOcc[rNum] += 1;
+    } else {
+      countOcc[rNum] = 1;
+    }
+  }
+
+  let total = 0;
+  for (let i = 0; i < data[0].length; i++) {
+    let left = data[0][i];
+    if (countOcc[left]) {
+      total += left * countOcc[left];
+    }
+  }
+  return total;
+}
+
+console.log(puzzle2());
