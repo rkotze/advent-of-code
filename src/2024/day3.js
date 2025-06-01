21e3d4358cf4f5a914caddedd602147f7c65fca2
Richard Kotze <richkotze@outlook.com>
2025-06-01 12:35:52 +0100

    Day 3.2 done

--- src/2024/day3.js
+++ src/2024/day3.js
@@ -17,3 +17,24 @@ function puzzle1() {
 }
 
 console.log(puzzle1());
+
+function puzzle2() {
+  const data = readPuzzle("2024", "day3.txt", (data) =>
+    data.replace(/(\r\n|\n|\r)/gm, "")
+  );
+  const filtered = data
+    .split("do()")
+    .map((str) => str.split("don't()")[0])
+    .join("");
+
+  const numList = Array.from(
+    filtered.matchAll(/mul\(([\d]{1,3},[\d]{1,3})\)/g),
+    (match) => {
+      const nums = match[1].split(",").map(Number);
+      return nums[0] * nums[1];
+    }
+  );
+  return numList.reduce((acc, num) => acc + num, 0);
+}
+
+console.log(puzzle2());
