6c152e6abc2669686d1a61c2ed0cd063f1c148f7
Richard Kotze <richkotze@outlook.com>
2025-06-18 01:28:27 +0100

    Day 5.2 solved

--- src/2024/day5.js
+++ src/2024/day5.js
@@ -32,3 +32,63 @@ function puzzle1() {
 }
 
 console.log(puzzle1());
+
+function validOrder(book, rules) {
+  for (let i = 0; i < book.length - 1; i++) {
+    const page = book[i];
+    const rest = book.slice(i + 1);
+    if (!rest.every((next) => rules.includes(page + "|" + next))) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function puzzle2() {
+  const [rules, books] = readPuzzle("2024", "day5.txt", (data) => {
+    const [rules, books] = data.split("\n\n");
+    return [
+      rules.split("\n"),
+      books.split("\n").map((pages) => pages.split(",").map(Number)),
+    ];
+  });
+
+  let total = 0;
+  const listInvalid = [];
+
+  for (let j = 0; j < books.length; j++) {
+    const book = books[j];
+
+    let valid = validOrder(book, rules);
+    if (!valid) listInvalid.push(book);
+  }
+
+  for (let j = 0; j < listInvalid.length; j++) {
+    const book = listInvalid[j];
+    let valid = true;
+
+    for (let i = 0; i < book.length - 1; i++) {
+      const page = book[i];
+      const rest = book.slice(i + 1);
+      for (let k = 0; k < rest.length; k++) {
+        const next = rest[k];
+        if (!rules.includes(page + "|" + next)) {
+          [book[i], book[i + (k + 1)]] = [book[i + (k + 1)], book[i]];
+          valid = false;
+        }
+      }
+      if (!valid) {
+        j -= 1;
+        break;
+      }
+    }
+
+    if (valid) {
+      total += book[Math.floor(book.length / 2)];
+    }
+  }
+
+  return total;
+}
+
+console.log(puzzle2());
